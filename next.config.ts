5f38714e22a219e6d177c0afbeccfbe4b31ed116
oreltol1000 <oreltol1000@users.noreply.github.com>
2025-10-21 13:58:24 +0300

    Chapter 11

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // enable Partial Prerendering(static rendering and dynamic rendering) for this route
+  cacheComponents: true,
 };
 
 export default nextConfig;
