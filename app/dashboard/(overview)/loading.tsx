ba6a5a82033c0e47943264c2f811e51071d41a50
oreltol1000 <oreltol1000@users.noreply.github.com>
2025-10-21 10:59:26 +0300

    chapter 10

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+export default function Loading() {
+  // we did the loading using suspense for each component
+  // return <DashboardSkeleton />;
+}
